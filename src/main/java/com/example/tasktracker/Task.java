db953bb579bea53df747369bf0f57ed1a24d37c7
DeanGuterman <deangut@gmail.com>
2025-04-28 19:29:27 +0300

    Add Task class

--- src/main/java/com/example/tasktracker/Task.java
+++ src/main/java/com/example/tasktracker/Task.java
@@ -1,8 +1,59 @@
-package com.exam
+package main.java.com.example.tasktracker;
 
-class Task{
-    String date;
-    TaskStatus status;
-    String content;
+import java.time.LocalDate;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Task {
+    private final long id;
+    private LocalDate dueDate;
+    private String title;
+    private String description;
+    private TaskStatus status;
+
+    public Task(long id, LocalDate dueDate, String title, String description){
+        this.id = id;
+        this.dueDate = dueDate;
+        this.title = title;
+        this.description = description;
+        this.status = INPROGRESS;
+    }
+
+    public long getId(){
+        return this.id;
+    }
+
+    public LocalDate getDueDate(){
+        return this.dueDate;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public TaskStatus getStatus(){
+        return this.status;
+    }
+
+    public void setDueDate(LocalDate dueDate){
+        this.dueDate = dueDate;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public void setStatus(TaskStatus status){
+        this.status = status;
+    }
+
+
 
 }
