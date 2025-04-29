6a998f84f20740c502590ae0dfbed39bef191025
DeanGuterman <deangut@gmail.com>
2025-04-29 13:41:02 +0300

    Make small changes for consistency, add requestmapping for taskcontroller class

--- src/main/java/com/example/tasktracker/TaskController.java
+++ src/main/java/com/example/tasktracker/TaskController.java
@@ -10,20 +10,21 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
 
-    private static AtomicLong id = new AtomicLong();
+    private static final AtomicLong idCounter = new AtomicLong();
     private static final List<Task> tasks = new ArrayList<>(){{
-        add(new Task(LocalDate.now(), "test author1", "test description1", TaskStatus.TODO, id.getAndIncrement()));
-        add(new Task(LocalDate.now(), "test author2", "test description2", TaskStatus.IN_PROGRESS, id.getAndIncrement()));
+        add(new Task(LocalDate.now(), "test title1", "test description1", TaskStatus.TODO, idCounter.getAndIncrement()));
+        add(new Task(LocalDate.now(), "test title2", "test description2", TaskStatus.IN_PROGRESS, idCounter.getAndIncrement()));
     }};
 
-    @GetMapping("/tasks")
+    @GetMapping
     public ResponseEntity<List<Task>> getTasks(){
         return new ResponseEntity<>(tasks, HttpStatus.OK);
     }
 
-    @GetMapping("/tasks/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Task> getTask(@PathVariable long id){
         return tasks.stream()
                 .filter(task -> task.getId() == id)
@@ -32,15 +33,16 @@ public class TaskController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    @PostMapping("/tasks")
-    public Task addTask(@RequestBody Task task){
-        Task newTask = new Task(task.getDueDate(), task.getTitle(), task.getDescription(), task.getStatus(), id.getAndIncrement());
+    @PostMapping
+    public ResponseEntity<Task> addTask(@RequestBody Task task){
+        Task newTask = new Task(task.getDueDate(), task.getTitle(), task.getDescription(), task.getStatus(), idCounter.getAndIncrement());
         tasks.add(newTask);
-        return newTask;
+        return new ResponseEntity<>(newTask, HttpStatus.CREATED);
     }
 
-    @DeleteMapping("/tasks/{id}")
-    public boolean deleteTask(@PathVariable long id){
-        return tasks.removeIf(task -> task.getId() == id);
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable long id){
+        boolean removed = tasks.removeIf(task -> task.getId() == id);
+        return removed ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
     }
 }
