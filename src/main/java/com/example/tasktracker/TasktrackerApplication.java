10afa9c8f7c0d058f7d797513d82d46dbdd7d78f
DeanGuterman <deangut@gmail.com>
2025-04-30 20:41:23 +0300

    Clean and update comments

--- src/main/java/com/example/tasktracker/TasktrackerApplication.java
+++ src/main/java/com/example/tasktracker/TasktrackerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
+// Main Spring Boot app entry point
 @SpringBootApplication
 public class TasktrackerApplication {
 
